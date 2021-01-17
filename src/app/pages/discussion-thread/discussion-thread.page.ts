581714bc816139f167e113337c9b96e8a2806f74
Hayden Bradfield <hbradfield20@gmail.com>
2021-01-16 21:04:28 -0500

    worked on displaying action dropdowns for discussion replies

--- src/app/pages/discussion-thread/discussion-thread.page.ts
+++ src/app/pages/discussion-thread/discussion-thread.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SideMenuService } from 'src/app/global-components/side-menu/side-menu.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
@@ -16,11 +18,14 @@ export class DiscussionThreadPage implements OnInit{
     public threadLoading: boolean = true;
     public repliesLoading: boolean = true;
     public threadId: string;
+    private determineActionsSubscription: Subscription;
+    public isLoggedIn: boolean;
 
     constructor(
         private crudService: CrudService, 
         private sideMenuService: SideMenuService, 
-        private route: ActivatedRoute
+        private route: ActivatedRoute, 
+        private authService: AuthService
     ){}
 
     ngOnInit(){
@@ -64,6 +69,7 @@ export class DiscussionThreadPage implements OnInit{
         .then((resp: any) => {
             console.log(resp);
             this.replies = resp.body.replies;
+            this.determineActionsForReplies();
         })
         .catch(err => {
             console.error(err);
@@ -73,9 +79,67 @@ export class DiscussionThreadPage implements OnInit{
         });
     }
 
+    private determineActionsForReplies(): void {
+
+        this.determineActionsSubscription = this.authService.user.subscribe(user => {
+
+            if(user){
+
+                this.isLoggedIn = true;
+
+                if(this.replies.length){
+
+                    const replyIds = this.replies.map(reply => reply.replyId);
+
+                    this.crudService.post(
+                        'discussions/actions-for-replies', 
+                        {replyIds}, 
+                        true
+                    )
+                    .toPromise()
+                    .then((resp: any) => {
+                        const actions = resp.body.actions;
+                        let repliesTmp = [...this.replies];
+    
+                        for(let action of actions){
+                            for(let reply of repliesTmp){
+                                if(action.replyId == reply.replyId){
+                                    reply.actions = action;
+                                }
+                            }
+                        }
+
+                        this.replies = repliesTmp;
+
+                        console.log(this.replies);
+    
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
+
+                }
+                
+            }else{
+                this.replies = this.replies.map(reply => {
+                    return {...reply, actions : null};
+                });
+                this.isLoggedIn = false;
+            }
+
+        });
+
+    }
+
     public openSideMenu(): void{
 
         this.sideMenuService.openSideMenu();
+
+    }
+
+    public toggleDropdown(dropdown: HTMLUListElement){
+
+        dropdown.style.display = dropdown.style.display == 'block' ? 'none' : 'block';
 
     }
 
